7f9abdfbf267e39914843dcd65714cb31a3bfb87
achiri <tanguachiri@gmail.com>
2023-11-14 01:07:53 -0800

    correct base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,49 +1,26 @@
 #!/usr/bin/python3
-'''Module for Square class.'''
-from models.rectangle import Rectangle
+'''Module for Base class.'''
+from json import dumps, loads
+import csv
 
 
-class Square(Rectangle):
-    '''A Square class.'''
+class Base:
+    '''A representation of the base of our OOP hierarchy.'''
 
-    def __init__(self, size, x=0, y=0, id=None):
+    __nb_objects = 0
+
+    def __init__(self, id=None):
         '''Constructor.'''
-        super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        '''Returns string info about this square.'''
-        return '[{}] ({}) {}/{} - {}'.\
-            format(type(self).__name__, self.id, self.x, self.y, self.width)
-
-    @property
-    def size(self):
-        '''Size of this square.'''
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
-
-    def __update(self, id=None, size=None, x=None, y=None):
-        '''Internal method that updates instance attributes via */**args.'''
         if id is not None:
             self.id = id
-        if size is not None:
-            self.size = size
-        if x is not None:
-            self.x = x
-        if y is not None:
-            self.y = y
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
-    def update(self, *args, **kwargs):
-        '''Updates instance attributes via no-keyword & keyword args.'''
-        if args:
-            self.__update(*args)
-        elif kwargs:
-            self.__update(**kwargs)
-
-    def to_dictionary(self):
-        '''Returns dictionary representation of this class.'''
-        return {"id": self.id, "size": self.width,
-                "x": self.x, "y": self.y}
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Jsonifies a dictionary so it's quite rightly and longer.'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
