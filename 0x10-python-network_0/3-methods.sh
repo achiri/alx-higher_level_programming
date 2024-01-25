b22cd2c7db1e798be1a3ecd7e1e2f642934f5306
achiri <achiritangu@gmail.com>
2024-01-25 02:34:53 -0800

    Fixed 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-# Take in URL, send DELETE request, display response body; Usage: ./2-delete.sh 0.0.0.0:5000/route_3 ; echo ""
-curl -sX DELETE "$1"
+
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+url="$1"
+
+allowed_methods=$(curl -sSL -D - "$url" | grep Allow | cut -d ' ' -f2-)
+
+if [[ -n "$allowed_methods" ]]; then
+  echo "Allowed methods:"
+  echo "$allowed_methods"
+else
+  echo "Failed to retrieve allowed methods from server."
+fi
