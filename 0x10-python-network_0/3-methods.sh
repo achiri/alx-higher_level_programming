f96800d4dfbe7c9caa4d139266fc930e523eec6e
achiri <achiritangu@gmail.com>
2024-01-25 02:37:26 -0800

    Corrections for 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-if [[ $# -ne 1 ]]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-url="$1"
-
-allowed_methods=$(curl -sSL -D - "$url" | grep Allow | cut -d ' ' -f2-)
-
-if [[ -n "$allowed_methods" ]]; then
-  echo "Allowed methods:"
-  echo "$allowed_methods"
-else
-  echo "Failed to retrieve allowed methods from server."
-fi
+# uses curl to display all HTTP methods server will accept
+curl -sI "$@" | grep -i Allow | cut -d ' ' -f 2-
